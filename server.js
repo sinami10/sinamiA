54bdc9d92637f9b43b228a1563b9fd4e86222eb9
unknown <sinami.amine7@gmail.com>
2020-07-08 11:41:07 +0100

    Initial heroku commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ang4-app'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/ang4-app/index.html'));});
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+ '/dist/index.html'));
+})
+
